393a67c4a9daddd036177cb8e8b7a32a8f2a5af2
Pranav Marathe <pmarathe25@gmail.com>
2018-11-09 19:12:44 -0800

    Adds nicer test runner output

--- include/core.hpp
+++ include/core.hpp
@@ -6,20 +6,19 @@
 #include <map>
 
 namespace Stealth::Test {
-    void log(const std::string& message) {
-        std::cerr << message << std::endl;
-    }
-
-    using TestFunc = std::function<void(void)>;
-
-    struct Test {
-        Test(TestFunc func = {}) : func{std::move(func)} { }
-        void operator()() const { func(); }
-        const TestFunc func;
+    class IndentedError {
+        public:
+            template <typename T>
+            std::ostream& operator<<(T&& val) {
+                return std::cerr << "\t" << val;
+            }
     };
 
+    #define LOG_MISMATCH(EXPECTED, VALUE) std::cerr << "[     ERROR]\n" << __FILE__ << ":" << __LINE__ << " " << "Value of: " << #VALUE << "\n  Actual: " << (VALUE) << "\nExpected: " << (EXPECTED) << std::endl;
+
     // Keep track of all tests.
-    std::map<const std::string, Test> tests{};
+    using TestFunc = std::function<void(bool&)>;
+    std::map<const std::string, TestFunc> tests{};
 
     // Must have a return type so that we can invoke this in the global scope.
     // The side-effect of this function is to register the test.
@@ -30,13 +29,19 @@ namespace Stealth::Test {
 
     // Tests
     #define STEST(NAME) \
-        void NAME(); \
+        void NAME(bool& __stest_passed); \
         namespace Stealth::Test { \
             [[maybe_unused]] const bool _ST_Func_Result_##NAME##_##discard = Stealth::Test::addTest(NAME, #NAME); \
         } \
-        void NAME()
+        void NAME(bool& __stest_passed)
 
     // Asserts and Expects
-    #define EXPECT_TRUE(CONDITION, MESSAGE) if (not (CONDITION)) log(#MESSAGE)
-    #define ASSERT_TRUE(CONDITION, MESSAGE) if (not (CONDITION)) log(#MESSAGE); return;
+    #define EXPECT_EQ(EXPECTED, VALUE) if (EXPECTED != VALUE) { LOG_MISMATCH(EXPECTED, VALUE); __stest_passed = false; } std::cerr
+    #define ASSERT_EQ(EXPECTED, VALUE) if (EXPECTED != VALUE) { LOG_MISMATCH(EXPECTED, VALUE); __stest_passed = false; return; }
+
+    #define EXPECT_TRUE(CONDITION) EXPECT_EQ(true, CONDITION)
+    #define ASSERT_TRUE(CONDITION) ASSERT_EQ(true, CONDITION)
+
+    #define EXPECT_FALSE(CONDITION) EXPECT_EQ(false, CONDITION)
+    #define ASSERT_FALSE(CONDITION) ASSERT_EQ(false, CONDITION)
 } /* Stealth::Test */
--- test/test.cpp
+++ test/test.cpp
@@ -1,19 +1,50 @@
 #include "interfaces/STest"
+#include <cassert>
 
-STEST(all_doNothing) {
-
+STEST(FailExpectTrue) {
+    EXPECT_TRUE(false);
 }
 
-STEST(all_doNothingAgain) {
-
+STEST(PassExpectTrue) {
+    EXPECT_TRUE(true);
 }
 
-STEST(all0_doNothing) {
-
+STEST(FailExpectFalse) {
+    EXPECT_FALSE(true);
 }
 
-STEST(al0_doNothingAgain) {
-
+STEST(PassExpectFalse) {
+    EXPECT_FALSE(false);
 }
 
-RUN_TESTS
+STEST(FailAssertTrue) {
+    ASSERT_TRUE(false);
+    assert(0); // This should not be reached.
+}
+
+STEST(PassAssertTrue) {
+    ASSERT_TRUE(true);
+}
+
+STEST(FailAssertFalse) {
+    ASSERT_FALSE(true);
+    assert(0); // This should not be reached.
+}
+
+STEST(PassAssertFalse) {
+    ASSERT_FALSE(false);
+}
+
+STEST(FailExpectEq) {
+    int a = 1, b = 2;
+    EXPECT_EQ(a, b);
+}
+
+STEST(PassExpectEq) {
+    int a = 1, b = 1;
+    EXPECT_EQ(a, b);
+}
+
+int main(int argc, char const *argv[]) {
+    RUN_STESTS(argc, argv);
+}
