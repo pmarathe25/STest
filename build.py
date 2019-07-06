ec0e39ba0a2d18aa6094e7ed3d422ec6ce6695fb
Pranav Marathe <pmarathe25@gmail.com>
2019-07-06 15:09:45 -0700

    Updates build script

--- build.py
+++ build.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import sbuildr
+import os
 project = sbuildr.Project()
 
 libstest = project.library("stest", sources=["STest.cpp"], libs=["stdc++"])
-project.install(libstest, "/usr/local/lib")
-project.install("STest.hpp", "/usr/local/include/Stealth")
+project.install(libstest, os.path.join("/", "usr", "local", "lib"))
+project.install("STest.hpp", os.path.join("/", "usr", "local", "include", "Stealth"))
 
 project.test("macros", sources=["macros.cpp"], libs=["stdc++", libstest])
 sbuildr.cli(project, default_profiles=["debug"])
